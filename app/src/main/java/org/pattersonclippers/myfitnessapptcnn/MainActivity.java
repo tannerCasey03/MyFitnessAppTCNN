e387cfd1941cfd953de4f1489c73f09fccde87f4
NJNV <Naglierinick57@gmail.com>
2024-03-05 14:17:43 -0500

    java code
    I finished the java

--- app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
+++ app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
@@ -3,12 +3,19 @@ package org.pattersonclippers.myfitnessapptcnn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button upperBTN,lowerBTN,cardBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        upperBTN = (Button) findViewById(R.id.upperBTN);
+        lowerBTN = (Button) findViewById(R.id.lowerBTN);
+        cardBTN = (Button) findViewById(R.id.cardBTN);
     }
 }
