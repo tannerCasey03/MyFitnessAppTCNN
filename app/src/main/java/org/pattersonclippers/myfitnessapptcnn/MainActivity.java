94d7499de90626c997020593c6cbabf31edb2b03
NJNV <Naglierinick57@gmail.com>
2024-03-06 13:49:30 -0500

    made a new activity
    Made new activity for upperbody

--- app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
+++ app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
@@ -2,7 +2,9 @@ package org.pattersonclippers.myfitnessapptcnn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
         upperBTN = (Button) findViewById(R.id.upperBTN);
         lowerBTN = (Button) findViewById(R.id.lowerBTN);
         cardBTN = (Button) findViewById(R.id.cardBTN);
+
+
+
+
+        upperBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, upperBodyActivity.class);
+                startActivity(myIntent);
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/org/pattersonclippers/myfitnessapptcnn/upperBodyActivity.java
@@ -0,0 +1,14 @@
+package org.pattersonclippers.myfitnessapptcnn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class upperBodyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upper_body);
+    }
+}
