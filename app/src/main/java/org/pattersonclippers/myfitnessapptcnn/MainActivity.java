b6b2374789cd7e9e382ea3ca40dace2b53150a53
tannerCasey03 <tannercasey261@gmail.com>
2024-04-03 14:29:41 -0400

    added calorie counter

--- app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
+++ app/src/main/java/org/pattersonclippers/myfitnessapptcnn/MainActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button upperBTN,lowerBTN,cardBTN;
+    Button upperBTN,lowerBTN,cardBTN,addBTN,resetBTN;
+    TextView totalCalTV;
+    EditText calorieET;
+    int total;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         upperBTN = (Button) findViewById(R.id.upperBTN);
         lowerBTN = (Button) findViewById(R.id.lowerBTN);
         cardBTN = (Button) findViewById(R.id.cardBTN);
+        addBTN = (Button) findViewById(R.id.addBTN);
+        resetBTN = (Button) findViewById(R.id.resetBTN);
+        totalCalTV = (TextView) findViewById(R.id.totalCalTV);
+        calorieET = (EditText) findViewById(R.id.calorieET);
+
+        total = 0;
 
 
 
@@ -53,6 +65,25 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
+        });
+
+
+        addBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int numsFromET = Integer.parseInt(String.valueOf(calorieET.getText()));
+                total += numsFromET;
+                totalCalTV.setText(total + "");
+            }
+        });
+
+
+        resetBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                total = 0;
+                totalCalTV.setText(total + "");
+            }
         });
 
     }
